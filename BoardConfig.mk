96752a5d454b58e2ccfccf6c3d4195a74ca06ca1
MLZ94 <zugutoroberto@gmail.com>
2021-04-08 23:34:33 +0000

    Switch to Busybox

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -67,7 +67,7 @@ TW_BRIGHTNESS_PATH := /sys/class/leds/lcd-backlight/brightness
 TW_MAX_BRIGHTNESS := 255
 TW_DEFAULT_BRIGHTNESS := 178
 TW_EXCLUDE_DEFAULT_USB_INIT := true
-TW_USE_TOOLBOX := true
+TW_INCLUDE_BUSYBOX := true
 
 # FS
 TW_INCLUDE_NTFS_3G := true
